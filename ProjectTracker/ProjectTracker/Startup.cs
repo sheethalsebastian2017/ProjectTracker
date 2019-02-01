88479bc3eb3f5d46384148efc259127ba030a9f6
sheethal sebastian <sheethalsebastian2017@gmail.com>
2019-01-31 21:48:13 -0500

    first commit

---
+++ ProjectTracker/ProjectTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectTracker.Startup))]
+namespace ProjectTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
